cc3dcfb1d3a56a3a20faca01e0b998794217eb3e
Koen Vendrik <k.vendrik@gmail.com>
2015-10-10 22:19:34 +0200

    Create file-drop.directive.js

---
+++ file-drop.directive.js
@@ -0,0 +1,49 @@
+angular
+  .module('ngFileDrop', [])
+
+  .directive('fileDrop', function(){
+    return {
+      	restrict: 'A',
+      	transclude: true,
+      	scope: {
+        	onDrop: '&',
+        	dragOverClass: '@'
+      	},
+
+    	link: function(scope, element, attrs, ctrl, transclude){
+        
+	      	var helper = {
+	            init: function(){
+	                var dropArea = element;
+
+	                var handleDrag = function(e){
+	                    e.preventDefault();
+	                    e.stopPropagation();
+
+	                    if(e.type === 'dragover'){
+	                        dropArea.addClass(scope.dragOverClass);
+	                    } else {
+	                        dropArea.removeClass(scope.dragOverClass);
+	                    }
+	                };
+
+	                var handleFileSelect = function(e){
+	                    handleDrag(e);
+	                    scope.onDrop({ file: e.target.files || e.dataTransfer.files });
+	                };
+
+	                dropArea.bind('dragover', handleDrag);
+	                dropArea.bind('dragleave', handleDrag);
+	                dropArea.bind('drop', handleFileSelect);
+	            }
+	        };
+
+	        if(window.File && window.FileList && window.FileReader){
+	            helper.init();
+	            transclude(function(clone){
+	                element.append(clone);  
+	            });
+	        }
+        }
+    };
+});
